766f8c3121bed62585e4ed71007abb41e07b8233
Dominik <kontakt.minik@gmail.com>
2023-04-26 09:46:27 +0200

    Resize divs height

--- script.js
+++ script.js
@@ -7,7 +7,10 @@ const passwordMatchInformation = document.querySelector(
 function checkPassword() {
   let password = document.querySelector("#password").value;
   let passwordConfirm = document.querySelector("#password-confirm").value;
-  if (password !== passwordConfirm) {
+  if (
+    password !== passwordConfirm ||
+    (password === "") & (passwordConfirm === "")
+  ) {
     passwordMatchInformation.textContent = "*Password do not match";
     passwordInput.classList.add("password__do-not-match");
     passwordConfirmInput.classList.add("password__do-not-match");
