f11fc02e7783094e4344a54576102eaa2438e788
Dominik <kontakt.minik@gmail.com>
2023-04-26 09:04:19 +0200

    Create and style wrong password warning

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-const password = document.querySelector("#password");
-const passwordConfirm = document.querySelector("#password-confirm");
+const passwordInput = document.querySelector("#password");
+const passwordConfirmInput = document.querySelector("#password-confirm");
 const passwordMatchInformation = document.querySelector(
   ".password__match-information"
 );
 
-if (password.textContent === passwordConfirm.textContent) {
-  passwordMatchInformation.textContent = "*Password do not match";
+function checkPassword() {
+  let password = document.querySelector("#password").value;
+  let passwordConfirm = document.querySelector("#password-confirm").value;
+  if (password !== passwordConfirm) {
+    passwordMatchInformation.textContent = "*Password do not match";
+    passwordInput.classList.add("password__do-not-match");
+    passwordConfirmInput.classList.add("password__do-not-match");
+  } else {
+    passwordMatchInformation.textContent = "";
+    passwordInput.classList.remove("password__do-not-match");
+    passwordConfirmInput.classList.remove("password__do-not-match");
+  }
 }
